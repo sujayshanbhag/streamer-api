202c8ad327a8628d73ea398c8c6037c659576329
SujayShanbhag <SujayShanbhag@users.noreply.github.com>
2025-10-27 21:08:20 +0530

    initial app

---
+++ src/main/java/com/example/demo/VideoStreamerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamerApplication.class, args);
+	}
+
+}
