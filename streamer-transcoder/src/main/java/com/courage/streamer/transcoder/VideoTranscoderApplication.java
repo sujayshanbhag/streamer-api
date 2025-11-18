1d6c475a1101f911bb7a2618aaf2e95861025ed9
Sujay Shanbhag <127194126+sujayshanbhag@users.noreply.github.com>
2025-11-18 23:28:21 +0530

    chore: created transcoder module

---
+++ streamer-transcoder/src/main/java/com/courage/streamer/transcoder/VideoTranscoderApplication.java
@@ -0,0 +1,12 @@
+package com.courage.streamer.transcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoTranscoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoTranscoderApplication.class, args);
+    }
+}
